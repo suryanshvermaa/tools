7d72fb9521cabe5582a31ffa475a3a585b951bcc
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-08-31 11:40:01 +0530

    updated

--- AutomatedTools/src/middlewares/multer.js
+++ AutomatedTools/src/middlewares/multer.js
@@ -3,7 +3,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.resolve("src/uploads/"));
+      cb(null, path.resolve(".","src","uploads"));
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
