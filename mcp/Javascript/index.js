2bd7da297ebba8101ec220a9cadd8d762f213255
Suryansh Verma <suryanshv.ug23.ee@nitp.ac.in>
2025-09-30 12:50:35 +0530

    Add weather data fetching functionality and include Zod as a dependency

--- mcp/Javascript/index.js
+++ mcp/Javascript/index.js
@@ -1,10 +1,32 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import {z} from "zod"
 
 const server=new McpServer({
     name: 'Weather Data Fetcher',
     version: '1.0.0',
 })
 
+async function getWeatherByCity(city='') {
+    if (city.toLocaleLowerCase()=='lakhimpur'){
+        return {temp:'30C',forcast:'chances of high rain'}
+    }
+    if (city.toLocaleLowerCase()=='sitapur'){
+        return {temp:'40C',forcast:'chances of high warm winds'}
+    }
+    return {temp:null,error:'unable to get the data'}
+}
+
+server.tool('getWeatherDataByCityName',{
+    city: z.string(),
+},async({city})=>{
+    return {content:[{type:"text",text: JSON.stringify(await getWeatherByCity(city))}]}
+})
 
 
+async function init() {
+    const transport=new StdioServerTransport();
+    await server.connect(transport);
+}
 
+init();
